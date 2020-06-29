8ef12d868a99b4a95cd4f8bb10eebec6d76f8d64
Willinson Bastos <willinson.bastos@gmail.com>
2020-06-29 09:56:02 -0300

    Adicionado 'boa noite'

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,8 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Bom dia");
 		System.out.println("Boa tarde");
-		
+		System.out.println("Boa noite");
 	}
-
 }
